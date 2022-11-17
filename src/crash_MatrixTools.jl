9c3a851a2ab6bbff177ed28850171f6019bf03b8
Ludovico-Urbani <lu.urbani@outlook.com>
2022-11-17 14:04:05 +0100

    Create crash_Pivot file

--- src/crash_MatrixTools.jl
+++ src/crash_MatrixTools.jl
@@ -1,36 +1,10 @@
 module crash_MatrixTools
 
 include("crash_Matrix.jl")
+export Matrix_IsNull, Matrix_SwapRows, Matrix_RemoveRows, Matrix_ClearEmptyRow
 
-export Pivot_GetIndexRow, Pivot_GetRowIndex_MinPivot_First, Matrix_IsNull, Matrix_SwapRows, Matrix_RemoveRows, Matrix_ClearEmptyRow
+include("crash_Pivot.jl")
+export Pivot_GetIndexRow, Pivot_GetRowIndex_MinPivot_First
 
-function Pivot_GetIndexRow(V::Vector{Float64})::Int64
-    if size(V, 1) == 0
-        throw(DimensionMismatch("Vector size must be greather thank 0!"))
-    end
-
-    for (i,v) in enumerate(V)
-        if v != 0.0
-            return i
-        end
-    end
-
-    return -1
-end
-
-function Pivot_GetRowIndex_MinPivot_First(M::Matrix{Float64})::Int64
-    MinPivot::Int64 = -1
-    MinPivot_Row::Int64 = -1
-
-    for r in 1:size(M,1)
-        p = Pivot_GetIndexRow(vec(M[[r], :]))
-        if p != -1 && (MinPivot < 0 || p < MinPivot)
-            MinPivot = p
-            MinPivot_Row = r
-        end
-    end
-
-    return MinPivot_Row
-end
 
 end # module crash_MatrixTools
