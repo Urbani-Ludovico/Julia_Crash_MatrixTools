9ce7c2a023875f974773146e85e5adc04f1c4193
Ludovico-Urbani <lu.urbani@outlook.com>
2022-11-18 16:00:54 +0100

    Snippet Matrix to Module

--- src/crash_Matrix.jl
+++ src/crash_Matrix.jl
@@ -1,50 +1,50 @@
-# module crash_Matrix
+module crash_Matrix
 
-include("crash_Pivot.jl")
+    include("crash_Pivot.jl")
 
-export Matrix_IsNull, Matrix_RemoveRows, Matrix_ClearEmptyRow, Matrix_IsInStair
+    export Matrix_IsNull, Matrix_RemoveRows, Matrix_ClearEmptyRow, Matrix_IsInStair
 
-function Matrix_IsNull(M::Matrix{Float64})::Bool
-    for i in eachindex(M) 
-        if M[i] != 0
+    function Matrix_IsNull(M::Matrix{Float64})::Bool
+        for i in eachindex(M) 
+            if M[i] != 0
+                return false
+            end
+        end
+        return true
+    end
+
+    function Matrix_RemoveRows(M::Matrix{Float64}, rows::Vector{Int64})::Matrix{Float64}
+        return M[[i for i in 1:size(M, 1) if i ∉ rows], :]
+    end
+
+    function Matrix_ClearEmptyRow(M::Matrix{Float64})::Matrix{Float64}
+        notEmpty = Vector{Int64}()
+        for r in 1:size(M,1)
+            row = M[[r],:]
+            if (count(!=(0), row) != 0)
+                append!(notEmpty, r)
+            end
+        end
+        return M[notEmpty, :];
+    end
+
+    function Matrix_IsInStair(M::Matrix{Float64})::Bool
+        if (Matrix_IsNull(M))
             return false
         end
-    end
-    return true
-end
 
-function Matrix_RemoveRows(M::Matrix{Float64}, rows::Vector{Int64})::Matrix{Float64}
-    return M[[i for i in 1:size(M, 1) if i ∉ rows], :]
-end
-
-function Matrix_ClearEmptyRow(M::Matrix{Float64})::Matrix{Float64}
-    notEmpty = Vector{Int64}()
-    for r in 1:size(M,1)
-        row = M[[r],:]
-        if (count(!=(0), row) != 0)
-            append!(notEmpty, r)
+        PivotCounter = -1
+        for r in 1:size(M,1)
+            p = crash_Pivot.Pivot_GetIndexRow(vec(M[[r], :]))
+            if p < 0
+                return false
+            elseif p > PivotCounter
+                PivotCounter = p
+            else
+                return false
+            end
         end
-    end
-    return M[notEmpty, :];
-end
-
-function Matrix_IsInStair(M::Matrix{Float64})::Bool
-    if (Matrix_IsNull(M))
-        return false
+        return true
     end
 
-    PivotCounter = -1
-    for r in 1:size(M,1)
-        p = Pivot_GetIndexRow(vec(M[[r], :]))
-        if p < 0
-            return false
-        elseif p > PivotCounter
-            PivotCounter = p
-        else
-            return false
-        end
-    end
-    return true
-end
-
-# end # crash_Matrix
+end # end crash_Matrix
