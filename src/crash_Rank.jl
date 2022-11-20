31dc27002fdc9b2be092e3c5a0b4b44cb10d5a99
Ludovico-Urbani <lu.urbani@outlook.com>
2022-11-20 16:29:32 +0100

    Create rank clac function with method choice

--- src/crash_Rank.jl
+++ src/crash_Rank.jl
@@ -3,7 +3,19 @@ module crash_Rank
     include("crash_Matrix.jl")
     include("crash_GaussStepping.jl")
 
-    function Rank_CalcMethod_Echelon(M::Matrix{Float64}) {
+    export Rank_Calc
+
+    Methods::Dict{String, Function} = Dict{String, Function}()
+
+    function Rank_Calc(M::Matrix{Float64}; method::String = "echelon")
+        if mathod in keys(Methods)
+            return Methods[method](M)
+        else
+            throw(ArgumentError("Method unknown"))
+        end
+    end
+
+    function Rank_CalcMethod_Echelon(M::Matrix{Float64})
         if crash_Matrix.Matrix_IsNull(M)
             return 0
         end
@@ -11,6 +23,7 @@ module crash_Rank
         M = crash_Matrix.Matrix_ClearEmptyRow(crash_GaussStepping.Gauss_Stepping(M))
 
         return size(M, 1)
-    }
+    end
+    Methods["echelon"] = Rank_CalcMethod_Echelon
 
 end # end crash_Rank
